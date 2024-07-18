5ddab3f7c5484185e8779504a5c13966af12690a
Giao Le <0xwinterchild@gmail.com>
2024-07-18 23:32:06 +0700

    feat(fe): Add home page's title, bg image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import Header from "@/components/home/header";
 import Main from "@/components/home/main";
 import Footer from "@/components/home/footer";
--- components/home/main.tsx
+++ components/home/main.tsx
@@ -1,5 +1,22 @@
+import { Button, Container, Divider, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import image from "/public/playing-piano-background.webp";
 export default function Main() {
-  return <main>
-    
-  </main>
+  return (
+    <Grid container spacing={5} className="relative w-screen h-screen">
+      <Grid item xs={6} className="flex items-center justify-center">
+        <Stack spacing={4} flexDirection="column" className="flex justify-center items-center" >
+          <Typography variant="h2" className="text-center">
+            Kết nối những niềm đam mê âm nhạc
+          </Typography>
+          <Button variant="contained" className="w-fit">
+            Đăng ký ngay
+          </Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={6}>
+        <Image src={image} alt="Piano" className="h-lvh" />
+      </Grid>
+    </Grid>
+  );
 }
