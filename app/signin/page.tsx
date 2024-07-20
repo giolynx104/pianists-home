fe1d60a793c2e7db0b48790b1d6f524de176be5d
Giao Le <0xwinterchild@gmail.com>
2024-07-20 17:01:24 +0700

    chore(login): password field hidden

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -19,67 +19,70 @@ import EmailIcon from "@mui/icons-material/Email";
 import pianoImage from "/public/login-background.jpg";
 import SignInWithGithubButton from "@/components/signin/github-sign-in";
 import SignInWithGoogleButton from "@/components/signin/google-sign-in";
-import KeyIcon from '@mui/icons-material/Key';
+import KeyIcon from "@mui/icons-material/Key";
 
 export default function LoginPage() {
   return (
-      <Container className="flex flex-row justify-center items-center h-screen">
-        <Card className="rounded-3xl flex flex-row justify-center items-center">
-          <CardContent className="flex flex-col p-12">
-            <Stack spacing={4}>
-              <Stack
-                spacing={1}
-                direction="row"
-                className="flex flex-row justify-center items-center"
-              >
-                <Image src={icon} alt="icon" width={20} />
-                <Typography variant="h6" className="text-center">
-                  Home of Pianists
-                </Typography>
-              </Stack>
-              <Typography variant="h3" className="text-center">
-                Welcome Back!
+    <Container className="flex flex-row justify-center items-center h-screen">
+      <Card className="rounded-3xl flex flex-row justify-center items-center">
+        <CardContent className="flex flex-col p-12">
+          <Stack spacing={4}>
+            <Stack
+              spacing={1}
+              direction="row"
+              className="flex flex-row justify-center items-center"
+            >
+              <Image src={icon} alt="icon" width={20} />
+              <Typography variant="h6" className="text-center">
+                Home of Pianists
               </Typography>
-              <Stack spacing={2} direction="column">
-                <SignInWithGoogleButton />
-                <SignInWithGithubButton />
-              </Stack>
-              <Divider variant="middle">or</Divider>
-              <Typography variant="subtitle2" className="text-center">
-                Please enter your details
-              </Typography>
-              <TextField
-                label="Email"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <TextField
-                label="Password"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <KeyIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Button className="normal-case font-bold" variant="contained">Sign In</Button>
             </Stack>
-          </CardContent>
-          <CardMedia>
-            <Image
-              src={pianoImage}
-              alt="background"
-              objectFit="contain"
-              width={500}
+            <Typography variant="h3" className="text-center">
+              Welcome Back!
+            </Typography>
+            <Stack spacing={2} direction="column">
+              <SignInWithGoogleButton />
+              <SignInWithGithubButton />
+            </Stack>
+            <Divider variant="middle">or</Divider>
+            <Typography variant="subtitle2" className="text-center">
+              Please enter your details
+            </Typography>
+            <TextField
+              label="Email"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailIcon />
+                  </InputAdornment>
+                ),
+              }}
             />
-          </CardMedia>
-        </Card>
-      </Container>
+            <TextField
+              label="Password"
+              type="password"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <KeyIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button className="normal-case font-bold" variant="contained">
+              Sign In
+            </Button>
+          </Stack>
+        </CardContent>
+        <CardMedia>
+          <Image
+            src={pianoImage}
+            alt="background"
+            objectFit="contain"
+            width={500}
+          />
+        </CardMedia>
+      </Card>
+    </Container>
   );
 }
