23bacf73d1cd18775d9d0bb40f515352db1dfa4c
Giao Le <0xwinterchild@gmail.com>
2024-07-20 15:32:13 +0700

    chore(fe): change password field icon

---
+++ app/signin/layout.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+//TODO: Implement the layout
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
