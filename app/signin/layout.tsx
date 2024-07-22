1c99f618aaaa67892469cd241ab2bcdfac025625
Giao Le <0xwinterchild@gmail.com>
2024-07-22 10:18:13 +0700

    feat: add link to logo

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,7 +1,5 @@
 import { Box } from "@mui/material";
 
-//TODO: Implement the layout
-
 export default function DashboardLayout({
   children,
 }: {
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,14 +1,15 @@
 import { GiGrandPiano } from "react-icons/gi";
 import { Box, Stack, Typography } from "@mui/material";
-
-//TODO: Add redirection to home page for the logo
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Stack spacing={1} direction="row" className="flex items-center">
-      <GiGrandPiano className="items-center" />
-      <Typography variant="subtitle1">Home of Pianists</Typography>
-    </Stack>
+    <Link href="/">
+      <Stack spacing={1} direction="row" className="flex items-center">
+        <GiGrandPiano className="items-center" />
+        <Typography variant="subtitle1">Home of Pianists</Typography>
+      </Stack>
+    </Link>
   );
 };
 
