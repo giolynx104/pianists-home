d52683839080aa18d5960dfadc4c43cd89f8c073
GiaoLe <giolynx104@gmail.com>
2024-08-19 15:57:33 +0700

    hotfix: revert configuration changes that made UI broken and remove some redundant configs

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,29 @@
-"use client";
-
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import Main from "@/components/group/home/main";
-import React from "react";
-
-export default function Home() {
-  return <Main />;
+import { Button, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import image from "/public/playing-piano-background.webp";
+import Link from "next/link";
+export default function Page() {
+  return (
+    <Grid container className="relative">
+      <Grid item xs={6} className="flex items-center justify-center">
+        <Stack
+          spacing={4}
+          flexDirection="column"
+          className="flex justify-center items-center"
+        >
+          <Typography variant="h2" className="text-center">
+            Connect, Learn, Play
+          </Typography>
+          <Link href="/dashboard">
+            <Button variant="contained" className="normal-case">
+              Go to Dashboard
+            </Button>
+          </Link>
+        </Stack>
+      </Grid>
+      <Grid item xs={6} className="flex justify-center">
+        <Image src={image} alt="Piano" className="h-lvh w-auto" />
+      </Grid>
+    </Grid>
+  );
 }
