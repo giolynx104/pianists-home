c4203e2690f82195da4492dc489bd8a098ff5447
GiaoLe <giolynx104@gmail.com>
2024-09-03 12:33:55 +0700

    Change LanguageSelector text color and remove unused media

--- app/[language]/(shared-default-layout)/_components/LanguageSelector.tsx
+++ app/[language]/(shared-default-layout)/_components/LanguageSelector.tsx
@@ -4,7 +4,6 @@ import { LanguageContext } from "@/app/i18n/LanguageContext";
 import { MenuItem, Select } from "@mui/material";
 import { usePathname, useRouter } from "next/navigation";
 import { useContext } from "react";
-
 export const LanguageSelector = () => {
   const language = useContext(LanguageContext);
   const router = useRouter();
@@ -16,6 +15,7 @@ export const LanguageSelector = () => {
       onChange={(e) => {
         router.push(pathname.replace(`${language}`, e.target.value));
       }}
+      className="text-white"
     >
       <MenuItem value="en">English</MenuItem>
       <MenuItem value="vi">Tiếng Việt</MenuItem>
